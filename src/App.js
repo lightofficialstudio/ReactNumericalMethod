0dc9756d4809c4834cafe4824af1b6e9810eda52
lightofficialstudio <light@MacBook-Pro-khxng-light.local>
2021-04-04 16:43:46 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import './bootstrap.min.css';
+import './font.css';
 
 function App() {
   return (
     <div className="App">
+      <nav class="site-header py-1 container d-flex flex-column flex-md-row justify-content-between">
+                <a href="index.html" class="py-3" d-none d-md-inline-block>หน้าหลัก</a>
+                <a href="portfolio.html" class="py-3" d-none d-md-inline-block>PORTFOLIO</a>
+                <a href="#" class="py-3" d-none d-md-inline-block>WORK</a>
+                <a href="#" class="py-3" d-none d-md-inline-block>CONTACT</a>
+            </nav>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <h2 class="text-white">React VS. Numerical Method</h2>
+          <p class="text-white">รีแอค นิวเมอร์</p>
       </header>
+      <body>
+        <div class="bg-dark">
+        </div>
+      </body>
+      <footer>
+        <p></p>
+      </footer>
     </div>
+   
   );
 }
 
